4fba5b81c70e6c4a7b283529be1fcd14dfb87c2f
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2016-12-10 17:37:25 +0100

    Driver script

---
+++ swi-expand.pl
@@ -0,0 +1,17 @@
+#!/home/jan/bin/swipl
+
+:- use_module(expand).
+:- use_module(library(main)).
+
+:- initialization main.
+
+main(Files) :-
+	maplist(expand, Files).
+
+expand(File) :-
+	get_time(T0),
+	format(user_error, 'Expanding ~w ... ', [File]),
+	expand(File, File),
+	get_time(T1),
+	T is T1-T0,
+	format(user_error, 'done (~3f sec)~n', [T]).
