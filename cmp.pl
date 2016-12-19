804d2e2f1eae3679a783c96bcd20e5c4f8c969dd
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2016-12-19 17:14:24 +0100

    Cannot read two files at the same time as that messes up the read state.

--- cmp.pl
+++ cmp.pl
@@ -4,25 +4,23 @@
 :- use_module(library(prolog_source)).
 
 compare(File1, File2) :-
-    absolute_file_name(File1, Path1,
-                       [ file_type(prolog),
-                         access(read)
-                       ]),
-    absolute_file_name(File2, Path2,
+    source_file_to_terms(File1, Terms1),
+    source_file_to_terms(File2, Terms2),
+    compare_terms(Terms1, Terms2, 0, E),
+    E == 0.
+
+source_file_to_terms(File, Terms) :-
+    absolute_file_name(File, Path,
                        [ file_type(prolog),
                          access(read)
                        ]),
     setup_call_cleanup(
         prepare(State),
         setup_call_cleanup(
-            prolog_open_source(Path1, In1),
-            setup_call_cleanup(
-                prolog_open_source(Path2, In2),
-                compare_streams(In1, In2, 0, Errors),
-                prolog_close_source(In2)),
-            prolog_close_source(In1)),
-        restore(State)),
-    Errors == 0.
+            prolog_open_source(Path, In),
+            read_source_terms(In, Terms),
+            prolog_close_source(In)),
+        restore(State)).
 
 prepare(state(Xref,SingleTon)) :-
     (   current_prolog_flag(xref, Xref)
@@ -43,27 +41,31 @@ restore(state(Xref,SingleTon)) :-
     ;   style_check(-singleton)
     ).
 
+read_source_terms(In, Terms) :-
+    at_end_of_stream(In),
+    !,
+    Terms = [].
+read_source_terms(In, [@(File:Line,H)|T]) :-
+    prolog_read_source_term(In, H, _Exp1, [syntax_errors(error)]),
+    source_location(File, Line),
+    read_source_terms(In, T).
 
-compare_streams(In1, In2, E0, E) :-
-    Options = [syntax_errors(error)],
-    prolog_read_source_term(In1, Term1, _Exp1, Options),
-    prolog_read_source_term(In2, Term2, _Exp2, Options),
-    (   Term1 =@= Term2
-    ->  E1 = E0
-    ;   print_message(error, cmp(diff(Term1, Term2))),
+compare_terms([], [], E, E).
+compare_terms([@(P0,H0)|T0], [@(_P1,H1)|T1], E0, E) :-
+    (   H0 =@= H1
+    ->  E1 is E0
+    ;   print_message(error, cmp(diff(P0, H0, H1))),
         E1 is E0+1
     ),
-    (   Term1 == end_of_file
-    ->  E = E1
-    ;   compare_streams(In1, In2, E1, E)
-    ).
+    compare_terms(T0, T1, E1, E).
+
 
 		 /*******************************
 		 *            MESSAGES		*
 		 *******************************/
 
-prolog:message(cmp(diff(Term1, Term2))) -->
-    [ 'MISMATCH:'-[], nl,
+prolog:message(cmp(diff(File:Line, Term1, Term2))) -->
+    [ 'MISMATCH: ~w:~w'-[File, Line], nl,
       '~q \\=@='-[Term1], nl,
       '~q'-[Term2]
     ].
