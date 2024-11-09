f9f64377a601826ca1750836b842f5bde5abbdaa
Jan Wielemaker <jan@swi-prolog.org>
2024-11-09 10:20:55 +0100

    Turn into pack

---
+++ pack.pl
@@ -0,0 +1,10 @@
+pack_version(2).
+name(reindent).
+version('0.2.0').
+title('Re-indent Prolog sources').
+keywords([tab, source, layout]).
+author('Jan Wielemaker', 'jan@swi-prolog.org' ).
+author('Jan Wielemaker', 'jan@swi-prolog.org' ).
+requires(prolog >= '9').
+home('https://github.com/JanWielemaker/reindent' ).
+download( 'https://github.com/JanWielemaker/reindent.git' ).
